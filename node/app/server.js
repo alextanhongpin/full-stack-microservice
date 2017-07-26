0f0db93d7b56b067cdf3bcea0eda96340e965a63
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-07-26 13:40:38 +0800

    Add graceful shutdown for node microservice

--- node/app/server.js
+++ node/app/server.js
@@ -10,5 +10,26 @@ app.get('/*', (req, res) => {
 const PORT = process.env.PORT || 8080
 const HOST = '0.0.0.0'
 
-app.listen(PORT, HOST)
+const server = app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
+
+// this function is called when you want the server to die gracefully
+// i.e. wait for existing connections
+const gracefulShutdown = () => {
+  console.log('Received kill signal, shutting down gracefully.')
+  server.close(() => {
+    console.log('Closed out remaining connections.')
+    process.exit()
+  })
+
+  setTimeout(function () {
+    console.error('Could not close connections in time, forcefully shutting down')
+    process.exit()
+  }, 10 * 1000)
+}
+
+// listen for TERM signal .e.g. kill
+process.on('SIGTERM', gracefulShutdown)
+
+// listen for INT signal e.g. Ctrl-C
+process.on('SIGINT', gracefulShutdown)
